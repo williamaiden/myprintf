cbeddc58fdf4e3a098b583d61a3505c03ab5b4db
Willian Aiden <williamaiden@qq.com>
2017-06-16 22:31:42 +0800

    Create printf_va_arg.cpp

---
+++ printf_va_arg.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <typeinfo.h>
+
+void printf_va_arg(int n, ...){
+	
+	int t;
+	va_list ap;
+	va_start(ap, n);
+	for (int i = 0; i < n; ++i){
+		t = va_arg(ap, int);
+		printf("%d", t);
+	}
+}
+
+int main(void){
+	int i = 0;
+	if (typeid(i).name() == typeid(int).name()){
+		printf("yes\n");
+	}
+	printf2(5, 1, 2, 3, 4, 5);
+	printf2(8, 1, 2, 3, 4, 5,6,7,8);
+	return 0;
+}
